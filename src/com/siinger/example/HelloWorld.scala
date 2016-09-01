5e5278edbf5f32ea331b8a3e15240884371756b0
akkaActorDemo <siinger@163.com>
2016-09-01 17:28:06 +0800

    -add basic

--- src/com/siinger/example/HelloWorld.scala
+++ src/com/siinger/example/HelloWorld.scala
@@ -1,5 +1,13 @@
 package com.siinger.example
 
+/**
+ * ClassName: HelloWorld <br/>
+ * date: 2016年9月1日 下午5:07:27 <br/>
+ *
+ * @author siinger
+ * @version 
+ * @since JDK 1.7
+ */
 object HelloWorld {
   
   def main(args:Array[String]){
